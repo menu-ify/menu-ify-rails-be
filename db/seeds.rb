cd4257cf166f8f2a093d76464e6b1de61056c06b
Emily Port <emilylport@gmail.com>
2023-02-03 22:17:33 -0700

    seed db with faker data for testing purposes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do 
+  restaurant = Restaurant.create(name: Faker::Restaurant.name, description: Faker::Restaurant.description, logo: Faker::Internet.url)
+  5.times do 
+    restaurant.menu_items.create(name: Faker::Food.dish, description: Faker::Food.description, allergens: Faker::Food.ingredient, category: Faker::Food.ethnic_category, image: Faker::Internet.url, price: Faker::Number.decimal(l_digits: 2, r_digits: 2) ) 
+  end
+end

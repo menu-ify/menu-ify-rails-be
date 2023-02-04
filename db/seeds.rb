df71916ea1f7eb6609604156b61e7179408f59cc
Emily Port <emilylport@gmail.com>
2023-02-04 11:40:31 -0700

    update seed data to reflect schema

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,6 @@
 5.times do 
   restaurant = Restaurant.create(name: Faker::Restaurant.name, description: Faker::Restaurant.description, logo: Faker::Internet.url)
   5.times do 
-    restaurant.menu_items.create(name: Faker::Food.dish, description: Faker::Food.description, allergens: Faker::Food.ingredient, category: Faker::Food.ethnic_category, image: Faker::Internet.url, price: Faker::Number.decimal(l_digits: 2, r_digits: 2) ) 
+    restaurant.menu_items.create(name: Faker::Food.dish, description: Faker::Food.description, tags: Faker::Food.ingredient, category: Faker::Food.ethnic_category, image: Faker::Internet.url, price: Faker::Number.decimal(l_digits: 2, r_digits: 2) ) 
   end
 end

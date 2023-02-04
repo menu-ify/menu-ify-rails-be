c95e64f39cf9fd0fb921cfb1ae56879f50ac75bd
Emily Port <emilylport@gmail.com>
2023-02-04 11:35:06 -0700

    add menuitem validations

--- spec/models/menu_item_spec.rb
+++ spec/models/menu_item_spec.rb
@@ -4,4 +4,15 @@ RSpec.describe MenuItem, type: :model do
   describe 'relationships' do 
     it { should belong_to :restaurant }
   end
+
+  describe 'validations' do 
+    it { should validate_uniqueness_of :name }
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :description }
+    it { should validate_presence_of :tags }
+    it { should validate_presence_of :category }
+    it { should validate_presence_of :image }
+    it { should validate_presence_of :price }
+  end
+
 end
